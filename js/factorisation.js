d88fb770fadeaedf5888c81e95796ff50989e2c4
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-15 21:09:39 +0100

    Implement factorisation

--- js/factorisation.js
+++ js/factorisation.js
@@ -1,3 +1,15 @@
 ;(function(factors){
     // implement factors.of
+    factors.of = function(n){
+        var result = [];
+        var d = 2;
+        while (n >= d) {
+            while (n % d == 0) {
+                result.push(d);
+                n = n/d;
+            }
+            d += 1;
+        }
+        return result;
+    }
 })(window.factors = window.factors || {});
--- spec/factorisationSpec.js
+++ spec/factorisationSpec.js
@@ -2,4 +2,19 @@ describe('factors', function(){
     it('should exist', function(){
         expect(factors).toBeDefined();
     });
+
+    describe('of', function(){
+        it('should determine prime factors', function(){
+            var expected = {
+                2 : [2],
+                4 : [2, 2],
+                3 : [3],
+                5 : [5],
+                6 : [2, 3]
+            };
+            for (var n in expected){
+                expect(factors.of(parseInt(n))).toEqual(expected[n]);
+            }
+        });
+    });
 });
