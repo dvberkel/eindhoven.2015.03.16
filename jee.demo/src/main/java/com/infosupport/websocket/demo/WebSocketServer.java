b70b3b38f9144efc17a74060a04fa52c943171b6
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-16 07:31:17 +0100

    Create @OnOpen handler

--- jee.demo/src/main/java/com/infosupport/websocket/demo/WebSocketServer.java
+++ jee.demo/src/main/java/com/infosupport/websocket/demo/WebSocketServer.java
@@ -1,7 +1,13 @@
 package com.infosupport.websocket.demo;
 
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/ping")
 public class WebSocketServer {
+    @OnOpen
+    public void receiveWebSocketSession(Session session) {
+        /* handle session */
+    }
 }
